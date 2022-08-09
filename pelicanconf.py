7313e9c55c9420cd7871ef2657fea6d97a2b6a62
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2022-08-09 23:06:11 +0200

    Update SITEURL

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ DESCRIPTION = (
     "a lo largo del país, fortaleciendo a los miembros de la comunidad para generar un impacto "
     "positivo en el desarrollo de Python a nivel nacional y mundial."
 )
-SITEURL = "."
+SITEURL = ""
 
 PATH = "content"
 
