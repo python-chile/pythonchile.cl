8f37829808734880c83b3e407f0c8e5a2f1efca6
Tony-Rome <tonyn.rome@gmail.com>
2025-10-01 20:45:45 -0300

    feat: config github page

---
+++ publishconf.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+#SITEURL = "https://www.pythonchile.cl"
+SITEURL = "https://pythonchile.cl.github.io"
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+
+DELETE_OUTPUT_DIRECTORY = True
