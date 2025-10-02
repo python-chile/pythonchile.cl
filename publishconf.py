492a7b31441919b6f4495155429789e6c1902771
Tony-Rome <tonyn.rome@gmail.com>
2025-10-01 22:41:55 -0300

    feat: site url

--- publishconf.py
+++ publishconf.py
@@ -5,7 +5,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 #SITEURL = "https://www.pythonchile.cl"
-SITEURL = "https://python-chile.github.io/pythonchile.cl/"
+SITEURL = "https://python-chile.github.io/"
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
