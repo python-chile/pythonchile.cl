02d469a4b1c1def891f21343702e59d239d18d2d
Tony-Rome <tonyn.rome@gmail.com>
2025-10-01 21:06:20 -0300

    fix: atoms feed

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,4 @@ from pelicanconf import *
 SITEURL = "https://python-chile.github.io/pythonchile.cl/"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
