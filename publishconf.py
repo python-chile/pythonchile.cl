151e29ff1d71874d3a6fa9e3d61f4c33933e4ce2
Tony-Rome <tonyn.rome@gmail.com>
2025-10-03 20:01:22 -0300

    fix: content path

--- publishconf.py
+++ publishconf.py
@@ -2,10 +2,8 @@ import os
 import sys
 
 sys.path.append(os.curdir)
-from pelicanconf import *
 
-#SITEURL = "https://www.pythonchile.cl"
-SITEURL = "https://python-chile.github.io"
+SITEURL = "https://python-chile.github.io/pythonchile.cl"
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
